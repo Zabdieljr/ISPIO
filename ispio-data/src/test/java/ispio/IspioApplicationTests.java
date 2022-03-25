9f5604beff669ba6252bebe68961fa168700fb0d
Zkario <zabdieljr@gmail.com>
2022-03-24 19:32:56 -0500

    added ISPIO Logo. Closes #10.

---
+++ ispio-data/src/test/java/ispio/IspioApplicationTests.java
@@ -0,0 +1,13 @@
+package ispio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IspioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
